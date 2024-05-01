ea4654fa3ceb8531097c39d0c648da011111eb2d
Will Elson <elson594@gmail.com>
2024-05-01 20:47:29 +0100

    Layout styles

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,19 +1,21 @@
 import { Outlet, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
+import styles from './styles/Layout.module.css';
+
 export default function Content() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/search'>Search</Link>
-          </li>
-          <li>
-            <Link to='/favourites'>Favourites</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.header}>
+        <div className={styles.navButtons}>
+          <Link to='/search'>Search</Link>
+          <Link to='/favourites'>Favourites</Link>
+        </div>
+        <div className={styles.userInfo}>
+          Logged in as bla@ma.com{' '}
+          <button style={{ marginLeft: '12px' }}>Logout</button>
+        </div>
+      </div>
 
       <Outlet />
     </>
